097a8c4aff731ce3cbad2e77d2da12556254dad8
sukhunkang <7sukhun7@gmail.com>
2023-06-07 15:55:30 +0900

    2023.06.07 자바 공부

---
+++ src/codingtest_level0/Exercise28.java
@@ -0,0 +1,9 @@
+package codingtest_level0;
+
+class Exercise28 {
+    public int Exercise28(double flo) {
+        int answer = 0;
+        answer = (int) Math.floor(flo);
+        return answer;
+    }
+}
