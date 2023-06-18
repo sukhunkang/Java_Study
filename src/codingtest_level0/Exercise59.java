23d58b2139778fc584115764235c81258f633e3b
sukhunkang <7sukhun7@gmail.com>
2023-06-18 09:49:33 +0900

    2023.06.18

---
+++ src/codingtest_level0/Exercise59.java
@@ -0,0 +1,9 @@
+package codingtest_level0;
+
+class Exercise59 {
+    public String solution(String rny_string) {
+        String answer = "";
+        answer = rny_string.replace("m","rn");
+        return answer;
+    }
+}
---
+++ src/codingtest_level0/Exercise61.java
@@ -0,0 +1,9 @@
+package codingtest_level0;
+
+class Exercise61 {
+    public String solution(String myString) {
+        String answer = "";
+        answer = myString.toUpperCase();
+        return answer;
+    }
+}
