9bff2ed22621021f45e19defd7fc43d9201b727f
sukhunkang <7sukhun7@gmail.com>
2023-08-18 21:31:26 +0900

    2023.08.18 자바 공부

--- src/codingtest_level1/Exercise1.java
+++ src/codingtest_level1/Exercise1.java
@@ -1,13 +1,18 @@
 package codingtest_level1;
 
 class Exercise1 {
-    public double solution(int[] arr) {
+    
+	
+	public double solution(int[] arr) {
         double answer = 0;
         int sum = 0;
+      
         for(int i=0; i<arr.length; i++) {
             sum += arr[i];
-        }
+        } // for
+        
         answer = (double) sum / arr.length;
         return answer;
-    }
-}
+    } // solution
+	
+} // end class
