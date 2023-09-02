3aa1d313dd9874a99360ea5f766b3b7f3482af05
sukhunkang <7sukhun7@gmail.com>
2023-09-02 17:04:25 +0900

    2023.09.02 자바 공부

---
+++ src/codingtest_level1/Exercise12.java
@@ -0,0 +1,12 @@
+package codingtest_level1;
+
+class Exercise12 {
+   
+	
+	public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    } // solution
+	
+} // end class
